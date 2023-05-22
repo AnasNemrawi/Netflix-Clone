dd75d2d10725ca7e2e29fc85c5f3c5a454edf5f4
AnasNemrawi <anasnemrawi3000@gmail.com>
2023-05-22 19:33:46 +0000

    lab19

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import MovieList from './Components/MovieList/MovieList'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import MovieList from './Components/MovieList/MovieList';
 import ModalMovie from './Components/ModalMovie/ModalMovie';
 import Row from 'react-bootstrap/Row';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';// Import Route from react-router-dom
+import FavList from './Components/FavList/FavList'; // Import FavList component
+import './App.css';
+
 
 function Home() {
   const [trendingMovies, setTrendingMovies] = useState([]);
@@ -22,12 +27,22 @@ function Home() {
   }, [])
   return (
     <div>
+      <header>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/FavList' element={<FavList />} />
+          </Routes>
+        </Router>
+      </header>
+
+
       <Row>
         <MovieList movies={trendingMovies} setShow={setShow} setTargetMovie={setTargetMovie} />
         <ModalMovie handleClose={handleClose} handleShow={handleShow} show={show} targetMovie={targetMovie} />
       </Row>
     </div>
-  )
+  );
 }
 
 export default Home
