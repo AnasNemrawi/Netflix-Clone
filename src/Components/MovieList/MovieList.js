cade5b1be8ca65e08538443a3ca908d2b922a506
Anas Nemrawi <129268571+AnasNemrawi@users.noreply.github.com>
2023-05-27 03:07:36 +0300

    Update MovieList.js

--- src/Components/MovieList/MovieList.js
+++ src/Components/MovieList/MovieList.js
@@ -1,18 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Movie from '../Movie/Movie';
+import axios from 'axios';
+import ModalMovie from '../ModalMovie/ModalMovie';
+
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Movie from '../Movie/Movie';
 
-function MovieList({ movies, setTargetMovie, setShow }) {
+function MovieList() {
+    const [trendingMovies, setTrendingMovies] = useState([]);
+    const [targetMovie, setTargetMovie] = useState({});
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    function getTrending() {
+        axios
+            .get('https://moviesapi-5cja.onrender.com/trending')
+            .then((res) => res.data)
+            .then((data) => setTrendingMovies(data.results))
+            .catch((err) => console.log(err));
+    }
+
+    useEffect(() => {
+        getTrending();
+    }, []);
+
     return (
-        <Row>
-            {movies.map(movie => (
-                <Col key={movie.id} sm={6} md={4} lg={3}>
-                    <Movie movie={movie} setTargetMovie={setTargetMovie} setShow={setShow} />
-                </Col>
+        <Container>
+            {trendingMovies.map((movie, index) => (
+                (index % 4 === 0) && (
+                    <Row key={`row-${index / 4}`}>
+                        <Col md={3}>
+                            <Movie
+                                movie={movie}
+                                setTargetMovie={setTargetMovie}
+                                setShow={setShow}
+                                key={movie.id}
+                            />
+                        </Col>
+                        {trendingMovies[index + 1] && (
+                            <Col md={3}>
+                                <Movie
+                                    movie={trendingMovies[index + 1]}
+                                    setTargetMovie={setTargetMovie}
+                                    setShow={setShow}
+                                    key={trendingMovies[index + 1].id}
+                                />
+                            </Col>
+                        )}
+                        {trendingMovies[index + 2] && (
+                            <Col md={3}>
+                                <Movie
+                                    movie={trendingMovies[index + 2]}
+                                    setTargetMovie={setTargetMovie}
+                                    setShow={setShow}
+                                    key={trendingMovies[index + 2].id}
+                                />
+                            </Col>
+                        )}
+                        {trendingMovies[index + 3] && (
+                            <Col md={3}>
+                                <Movie
+                                    movie={trendingMovies[index + 3]}
+                                    setTargetMovie={setTargetMovie}
+                                    setShow={setShow}
+                                    key={trendingMovies[index + 3].id}
+                                />
+                            </Col>
+                        )}
+                    </Row>
+                )
             ))}
-        </Row>
+            <ModalMovie
+                handleClose={handleClose}
+                handleShow={handleShow}
+                show={show}
+                targetMovie={targetMovie}
+            />
+        </Container>
     );
 }
 
 export default MovieList;
+
