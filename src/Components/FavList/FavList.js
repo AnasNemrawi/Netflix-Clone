dfd0151a43e1781797cddbc0a107dae513fbf37d
Anas Nemrawi <129268571+AnasNemrawi@users.noreply.github.com>
2023-05-27 03:05:55 +0300

    Update FavList.js

--- src/Components/FavList/FavList.js
+++ src/Components/FavList/FavList.js
@@ -1,68 +1,93 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Movie from '../Movie/Movie';
+import ModalMovie from '../ModalMovie/ModalMovie';
 
-import { useState, useEffect } from 'react'
-import { Card, Col, Container, Row } from 'react-bootstrap'
-import { Button } from 'react-bootstrap'
+function Favlist() {
+    const [movies, setMovies] = useState([]);
+    const [targetMovie, setTargetMovie] = useState({});
+    const [show, setShow] = useState(false);
 
-function FavList() {
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-    const [favoriteMovie, setFavoriteMovie] = useState()
-
-    async function getFavoriteMove() {
-        let url = `${process.env.REACT_APP_SERVER}/getallMovies`
-        let response = await fetch(url)
-        let favoriteMovie = await response.json()
-        setFavoriteMovie(favoriteMovie)
-
-    }
-
-    async function handelDelete(id) {
-        let url = `${process.env.REACT_APP_SERVER}/deleteMovie/${id}`
-        let response = await fetch(url, {
-            method: 'DELETE',
-        })
-
-        if (response.status === 200) { getFavoriteMove(); alert('Successfully Deleted') }
-
+    function getMovies() {
+        axios
+            .get(`https://moviesapi-5cja.onrender.com`)
+            .then((res) => {
+                setMovies(res.data);
+                console.log(res);
+            })
+            .catch((err) => console.log(err));
     }
 
     useEffect(() => {
-        getFavoriteMove()
-    }, [])
+        getMovies();
+    }, []);
 
+    function handleDelete(target) {
+        setTargetMovie(target);
+        axios
+            .delete(`https://moviesapi-5cja.onrender.com/movies/${target.id}`)
+            .then((res) => {
+                console.log(res);
+                setMovies(movies.filter((e) => e.id !== target.id));
+            })
+            .catch((err) => console.log(err));
+    }
+
+    function handleUpdate(e, target) {
+        setTargetMovie(target);
+        axios
+            .put(`https://moviesapi-5cja.onrender.com/movies/${target.id}`, {
+                comments: e.target.value,
+            })
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+    }
+
+    // Divide movies into rows with a specific number of columns
+    function divideMoviesIntoRows(movies, columns) {
+        const rows = [];
+        const totalMovies = movies.length;
+        const rowsCount = Math.ceil(totalMovies / columns);
+
+        for (let i = 0; i < rowsCount; i++) {
+            const rowMovies = movies.slice(i * columns, (i + 1) * columns);
+            const row = (
+                <div key={`row-${i}`} style={{ display: 'flex', marginBottom: '20px' }}>
+                    {rowMovies.map((movie) => (
+                        <Movie
+                            movie={movie}
+                            key={movie.id}
+                            setTargetMovie={setTargetMovie}
+                            setShow={setShow}
+                            setMovies={setMovies}
+                            onFav={true}
+                            handleDelete={handleDelete}
+                        />
+                    ))}
+                </div>
+            );
+            rows.push(row);
+        }
+
+        return rows;
+    }
 
     return (
-        <Container>
-            <Row>
-                {favoriteMovie && favoriteMovie.map(eachCard => {
-                    return (
-                        <Col xs={12} md={3} style={{ padding: 20 }}>
-                            <Card className="text-center" key={eachCard.id} >
-                                <Card.Header>{eachCard.title}</Card.Header>
-                                <Card.Body>
-                                    {/* <Card.Img src={eachCard.poster_path} alt={'image here '} /> */}
-                                    <Card.Text>Id :
-                                        {eachCard.id}
-                                    </Card.Text>
-                                    <Card.Text> MY Comment :
-                                        {
-                                            eachCard.myrate ? eachCard.myrate : 'No comments '
-                                        }
-                                    </Card.Text>
-
-                                    <Button variant="danger" onClick={() => { handelDelete(eachCard.id) }} >Delete</Button>
-                                </Card.Body>
-
-                            </Card>
-                        </Col>
-
-                    )
-                })}
-            </Row>
-        </Container>
-    )
+        <div>
+            {divideMoviesIntoRows(movies, 3)}
+            <ModalMovie
+                handleUpdate={handleUpdate}
+                handleClose={handleClose}
+                handleShow={handleShow}
+                show={show}
+                targetMovie={targetMovie}
+            />
+        </div>
+    );
 }
 
-export default FavList
-
+export default Favlist;
 
