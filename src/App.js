54510fa1610638178db7da64f388fb66648a6cbe
Anas Nemrawi <129268571+AnasNemrawi@users.noreply.github.com>
2023-05-27 03:05:29 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import Navbar from './Components/Navbar/Navbar';
 import './App.css';
 import Home from './Components/Home/Home';
-import Navbar from './Components/Navbar/Navbar'
-import { Route, Routes, BrowserRouter } from 'react-router-dom'
-
+import MovieList from './Components/MovieList/MovieList';
+import Favlist from './Components/FavList/FavList';
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter className="App">
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movieList" element={<MovieList />}></Route>
+        <Route path='/favList' element={<Favlist />}></Route>
       </Routes>
     </BrowserRouter>
-
   );
 }
-export default App;
 
+export default App;
